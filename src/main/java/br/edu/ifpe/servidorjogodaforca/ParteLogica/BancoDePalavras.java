d1ea90c4589c829673a3b73ececb3e936526acb3
Guilherme Silva <guilherme.dna06@gmail.com>
2017-09-18 11:23:29 -0300

    Adicionada lista e valores iniciais para a mesma

--- src/main/java/br/edu/ifpe/servidorjogodaforca/ParteLogica/BancoDePalavras.java
+++ src/main/java/br/edu/ifpe/servidorjogodaforca/ParteLogica/BancoDePalavras.java
@@ -1,9 +1,23 @@
 package br.edu.ifpe.servidorjogodaforca.ParteLogica;
+
 import java.util.ArrayList;
 
 public class BancoDePalavras {
 
+    protected ArrayList<String> Palavras = new ArrayList<String>();
+
     public BancoDePalavras() {
+        Palavras.add("LEITE");
+        Palavras.add("QUEIJO");
+        Palavras.add("BANANA");
+        Palavras.add("CAJU");
+        Palavras.add("GOAIBA");
+        Palavras.add("TOMATE");
+        Palavras.add("ARROZ");
+        Palavras.add("LARANJA");
+        Palavras.add("CARNE");
+        Palavras.add("BISCOITO");
+        Palavras.add("BOLACHA");
     }
 
 }
