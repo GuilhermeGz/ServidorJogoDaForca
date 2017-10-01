04c3e7a081275c814375d1940b6a8331beef394d
Guilherme Silva <guilherme.dna06@gmail.com>
2017-10-01 18:01:24 -0300

    Adicionado atributos que representam acertos e erros do jogador

--- src/main/java/br/edu/ifpe/servidorjogodaforca/ParteLogica/Jogo.java
+++ src/main/java/br/edu/ifpe/servidorjogodaforca/ParteLogica/Jogo.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 
 public class Jogo {
 
+    int erros;
+    int acertos;
     BancoDePalavras bd;
     char[] letras;
     int[] marcasao;
     ArrayList<Character> letrasRepetidas;
-    
+
     public String criarCampo(char[] lista, int[] marcador) {
         String campo = new String();
         for (int i = 0; i < lista.length; i++) {
