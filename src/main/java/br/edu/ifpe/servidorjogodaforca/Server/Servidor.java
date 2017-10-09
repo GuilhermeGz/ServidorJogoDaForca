abcbc7db56ffcc6ab5620dbd369a7dc412dff649
Guilherme Silva <guilherme.dna06@gmail.com>
2017-10-09 07:39:47 -0300

    Modo 2 clientes

--- src/main/java/br/edu/ifpe/servidorjogodaforca/Server/Servidor.java
+++ src/main/java/br/edu/ifpe/servidorjogodaforca/Server/Servidor.java
@@ -5,29 +5,76 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Servidor {
 
+    private int porta;
+    private ArrayList<PrintStream> clientes;
+    private ServerSocket servidor;
+    private ArrayList<Scanner> teclados;
+
+    public Servidor(int porta) throws IOException {
+        this.porta = porta;
+        this.clientes = new ArrayList<PrintStream>();
+        this.servidor = new ServerSocket(this.porta);
+        this.teclados = new ArrayList<Scanner>();
+    }
+
+    public void distribuirMensagens(String msg) {
+        for (PrintStream cliente : this.clientes) {
+            cliente.println(msg);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Jogo j = new Jogo();
-        ServerSocket servidor = new ServerSocket(32154);
+        Servidor server = new Servidor(32154);
         System.out.println("Porta 32154 aberta!");
         while (true) {
 
-            Socket cliente = servidor.accept();
+            Socket cliente = server.servidor.accept();
             System.out.println("Nova conexão com o cliente "
                     + cliente.getInetAddress().getHostAddress());
+            PrintStream ps = new PrintStream(cliente.getOutputStream());
+            Scanner teclado = new Scanner(cliente.getInputStream());
+            server.teclados.add(teclado);
+            server.clientes.add(ps);
+
             Thread t1 = new Thread(new Runnable() {
 
                 public void run() {
                     try {
-                        Scanner teclado = new Scanner(cliente.getInputStream());
+                        Scanner sc = new Scanner(cliente.getInputStream());
                         PrintStream saida = new PrintStream(cliente.getOutputStream());
-
+                        saida.println("Você está na fila automática do multiplayer, para sair dela"
+                                + " escolha a opção: Sair do modo online");
                         do {
+
+                            if (server.clientes.size() == 2) {
+                                server.distribuirMensagens("\nIniciado Modo de 2 Jogadores!! Surprise");
+                                j.setErros(0);
+                                j.setAcertos(0);
+                                j.inicializarAtributos(1, j);
+                                int i = 0;
+
+                                do {
+                                    server.distribuirMensagens(j.criarCampo(j.getLetras(), j.getMarcasao()));
+                                    server.distribuirMensagens("\nEscolha uma letra");
+                                    server.distribuirMensagens("Erros :" + j.getErros());
+                                    i += 15;
+
+                                    if (i % 2 == 0) {
+                                        i = 0;
+                                    } else {
+                                        i = 1;
+                                    }
+                                } while (j.escolherLetra(server.teclados.get(i).next(), j.getLetrasRepetidas()) == true);
+                                server.distribuirMensagens(j.verificarRsultadoDaPartida(j.getAcertos(), j.getLetras()));
+                            }
                             saida.println(j.mostrarMenu());
-                            int escrita = Integer.parseInt(teclado.next());
+                            int escrita = Integer.parseInt(sc.next());
                             j.inicializarAtributos(escrita, j);
 
                             if (j.getOpcao() == 1) {
@@ -39,25 +86,31 @@ public class Servidor {
                                     saida.println("\nEscolha uma letra");
                                     saida.println("Erros :" + j.getErros());
 
-                                } while (j.escolherLetra(teclado.next(), j.getLetrasRepetidas()) == true);
+                                } while (j.escolherLetra(sc.next(), j.getLetrasRepetidas()) == true);
                                 saida.println(j.verificarRsultadoDaPartida(j.getAcertos(), j.getLetras()));
 
                             } else if (j.getOpcao() == 2) {
                                 saida.println("Digite a palavra");
-                                saida.println(j.adicionarPalavra(teclado.next()));
+                                saida.println(j.adicionarPalavra(sc.next()));
                             } else if (j.getOpcao() == 3) {
                                 saida.println("Digite a palavra");
-                                j.removerPalavra(teclado.next());
+                                j.removerPalavra(sc.next());
                             } else if (j.getOpcao() == 4) {
                                 saida.println(j.mostrarLista());
-                            } else if (j.getOpcao() != 5) {
-                                saida.println("\nValor inapropriadoz\n");
+                            } else if (j.getOpcao() == 5) {
+                                if (server.clientes.get(0) == null) {
+                                    saida.print("\nNão é possível realizar a operação\n");
+                                }
+                                saida.println("\nRetirado da fila de multiplayer\n");
+                                server.clientes.remove(0);
+                                server.teclados.remove(0);
+                            } else if (j.getOpcao() == 6) {
+                                saida.println("\nValor inapropriado\n");
                             }
 
-                        } while (j.getOpcao() != 5);
+                        } while (j.getOpcao() != 6);
 
-                        teclado.close();
-                        servidor.close();
+                        sc.close();
                         cliente.close();
                     } catch (Exception e) {
                     }
